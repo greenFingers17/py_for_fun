eb3b169eeef2bed43ddffc3b7334eea6721abe5e
Inna Dolina <dolina8inna@gmail.com>
2025-07-16 11:19:02 +0200

    Added different types of error handling

--- error_handling.py
+++ error_handling.py
@@ -1,7 +1,14 @@
 print('How many cats do you have?')
 numCats = input()
-if numCats >= 4:
-    print('That is a lot of cats!')
-else:
-    print('That is not many.')
+try:
+    if int(numCats) >= 4:
+        print('That is a lot of cats!')
+    elif int(numCats) < 0:
+        raise NameError('The number of cats cannot be a negative value.')
+    elif int(numCats) == 0:
+        print('Seems like you do not have any cats at all!')
+    else:
+        print('That is not many.')
+except ValueError:
+    print('Please make sure to enter a number.')
 
